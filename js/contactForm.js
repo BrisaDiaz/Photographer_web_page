2dd5c86945c0b7c2b311ca09ad1b98029b916f80
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-02-25 14:15:31 -0300

    fixing contact form

--- js/contactForm.js
+++ js/contactForm.js
@@ -2,7 +2,9 @@ const d = document;
 
 export default function contactForm() {
   const $form = d.querySelector(".contact-form"),
-    $inputs = d.querySelectorAll(".contact-form *[required]");
+    $inputs = d.querySelectorAll(
+      ".contact-form__input , .contact-form__textarea"
+    );
 
   $inputs.forEach((input) => {
     const $span = d.createElement("span");
@@ -12,37 +14,57 @@ export default function contactForm() {
     input.insertAdjacentElement("afterend", $span);
   });
 
-  d.addEventListener("keyup", validateInputs);
+  d.addEventListener("keyup", handleKeyDown);
+  d.addEventListener("submit", handleSubmit);
+}
+function handleSubmit(e) {
+  const $form = d.querySelector(".contact-form"),
+    $inputs = d.querySelectorAll(
+      ".contact-form__input , .contact-form__textarea"
+    );
+  e.preventDefault();
+  if (e.target === $form) {
+    $inputs.forEach((input) => {
+      let pattern = input.pattern || input.dataset.pattern;
+      validateInput(input, pattern);
+    });
+  }
 }
 
-function validateInputs(e) {
-  if (e.target.matches(".contact-form [required]")) {
-    let $input = e.target,
-      pattern = $input.pattern || $input.dataset.pattern;
+function checkIsFormInput(e) {
+  return e.target.matches(".contact-form__input , .contact-form__textarea")
+    ? true
+    : false;
+}
+function handleKeyDown(e) {
+  if (!checkIsFormInput(e)) return;
+  const $input = e.target,
+    pattern = $input.pattern || $input.dataset.pattern;
 
-    if (pattern && $input.value !== "") {
-      let regex = new RegExp(pattern);
+  validateInput($input, pattern);
+}
+function validateInput($input, pattern) {
+  if (pattern && $input.value !== "") {
+    let regex = new RegExp(pattern);
 
-      return !regex.exec($input.value)
-        ? d
-            .getElementById($input.name)
-            .nextElementSibling.classList.add("contact-form__error--visible")
-        : d
-            .getElementById($input.name)
-            .nextElementSibling.classList.remove(
-              "contact-form__error--visible"
-            );
-    }
-    if (!pattern) {
-      return $input.value === ""
-        ? d
-            .getElementById($input.name)
-            .nextElementSibling.classList.add("contact-form__error--visible")
-        : d
-            .getElementById($input.name)
-            .nextElementSibling.classList.remove(
-              "contact-form__error--visible"
-            );
-    }
+    return !regex.exec($input.value) ? addError($input) : removeError($input);
   }
+  if (!pattern) {
+    return $input.value === "" ? addError($input) : removeError($input);
+  }
+}
+function addError(input) {
+  d.getElementById(input.name).nextElementSibling.classList.add(
+    "contact-form__error--visible"
+  );
+
+  input.classList.add("contact-form__input--invalid");
+}
+
+function removeError(input) {
+  d.getElementById(input.name).nextElementSibling.classList.remove(
+    "contact-form__error--visible"
+  );
+
+  input.classList.remove("contact-form__input--invalid");
 }
